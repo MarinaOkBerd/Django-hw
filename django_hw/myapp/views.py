2f5baa10ea5cb0ad13044a38f52bc03aa025b570
Marina Berdnikova <berdni4ka@rambler.ru>
2024-03-11 23:09:22 +0300

    Add homework 1

---
+++ django_hw/myapp/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    html = """<!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Home</title>
+        </head>
+        <body>
+        <p style="text-align: center">Home</p>
+        </body>
+        </html>"""
+    return HttpResponse(html)
+
+
+def about(request):
+    html = """<!doctype html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>About</title>
+        </head>
+        <body>
+        <p style="text-align: center">Page about</p>
+        </body>
+        </html>"""
+    return HttpResponse(html)
